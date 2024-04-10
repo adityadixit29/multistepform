13a55533bef91c6de9ef78607e3eecdd93673eab
adityadixit29 <118058546+adityadixit29@users.noreply.github.com>
2024-04-10 22:12:27 +0530

    Update Math.jsx

--- src/pages/Math.jsx
+++ src/pages/Math.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Progress from '../components/Progress'
 import Description from "../components/Description"
 import Mathcomponent from '../components/Mathcomponent'
 import ButtonComponent from '../components/ButtonComponent'
+import Button from '../components/Button'
 const Math = () => {
+  const [clicked, setclicked] = useState(false);
+  const handleclick = ()=>{
+    setclicked(true);
+    // console.log(clicked);
+  }
   return (
     <div>
       <Progress complete="80" show={true} prevpage="/rightplace" />
@@ -11,14 +17,16 @@ const Math = () => {
       <Description title="What is your math comfort level?" des="
       Choose the highest level you feel confidence in - you can always adjust later. "/>
       </div>
-      <div className='p-5 flex flex-wrap gap-3 justify-center'>
+     <button onClick={handleclick} className='w-[100vw]'>
+     <div className='p-5 flex flex-wrap gap-3 justify-center'>
       <Mathcomponent img="arithematic.png" title="Arithematic" subtitle="Introductory" width="85"/>
       <Mathcomponent img="BasicAlgebra.png" title="Basic Algebra" subtitle="Foundational" width="112"/>
       <Mathcomponent img="IntermediateAlgebra.png" title="Intermediate Algebra" subtitle="Intermediate" width="158"/>
       <Mathcomponent img="calculas.png" title="Calculas" subtitle="Advanced" width="85"/>
       </div>
+     </button>
       <div className='sm:mt-10'>
-     <ButtonComponent page="/ontheway"/>
+      {clicked ? <ButtonComponent page="/ontheway"/> : <Button/>}
      </div>
     </div>
   )
